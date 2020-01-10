dae20283e7c559abaaba4ace6657d10280326627
akshay <akshay.prabhu@robosoftin.com>
2020-01-10 16:18:42 +0530

    Application code changed

---
+++ DemoModules/app/src/main/java/com/android/example/demomodules/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.android.example.demomodules;
+
+import com.android.example.base.BaseApplication;
+
+public class DemoApplication extends BaseApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    protected void initDagger() {
+        super.initDagger();
+    }
+}
---
+++ DemoModules/app/src/main/java/com/android/example/demomodules/dagger/DemoAppComponent.java
@@ -0,0 +1,8 @@
+package com.android.example.demomodules.dagger;
+
+import dagger.Component;
+
+@Component(modules = {})
+@DemoApplicationScope
+public interface DemoAppComponent {
+}
